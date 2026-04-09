a7f277daa099f9aaf78c3e3d7ddf37b1a86e1b5d
Vikash <vkchaurasiyatrade@gmail.com>
2026-04-09 14:32:42 +0530

    fixed bug

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -4,10 +4,7 @@ class DioClient {
   static final Dio _dio = Dio(
     BaseOptions(
       baseUrl: "http://127.0.0.1:8000/api/",
-      headers: {
-        "Authorization":
-            "Token inv-264af3e031dd43b91f7f93b6152a3e0f7dec2554-20260406",
-      },
+
       connectTimeout: const Duration(seconds: 5),
       receiveTimeout: const Duration(seconds: 3),
     ),
